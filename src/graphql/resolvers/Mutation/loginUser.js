a5488d7b71692db16d5397e09816b0b5d30db8d1
karo-gol <golba.karolina@gmail.com>
2020-05-12 18:32:43 +0200

    changes in loginUser resolver

--- src/graphql/resolvers/Mutation/loginUser.js
+++ src/graphql/resolvers/Mutation/loginUser.js
@@ -1,6 +1,6 @@
 import { compare } from "bcryptjs";
 import { createRefreshToken, createAccessToken } from "#root/helpers/jwt/auth";
-import { sendRefreshToken } from "#root/helpers/jwt/sendRefreshToken";
+import sendRefreshToken from "#root/helpers/jwt/sendRefreshToken";
 import { User } from "#root/db/models/user";
 
 const loginUserResolver = async (obj, { login, password }, context) => {
